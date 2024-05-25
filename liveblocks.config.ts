dd88e63ce244672e6558fdbda933faecb8f0b8f8
dinesh <dinesh.m@edvoy.com>
2024-05-25 21:03:39 +0530

    Liveblocks authorization for room

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,8 +2,9 @@ import { createClient } from '@liveblocks/client';
 import { createRoomContext, createLiveblocksContext } from '@liveblocks/react';
 
 const client = createClient({
-  publicApiKey:
-    'pk_dev_Py8NecVZgGClNXiTSQpQ-pXMfSAQDGq7_2M8tbIstYe89yNAVRbOjo3IGfunr4q3',
+  authEndpoint: '/api/liveblocks-auth',
+  // publicApiKey:
+  //   'pk_dev_Py8NecVZgGClNXiTSQpQ-pXMfSAQDGq7_2M8tbIstYe89yNAVRbOjo3IGfunr4q3',
   async resolveUsers({ userIds }) {
     // Used only for Comments and Notifications. Return a list of user information
     // retrieved from `userIds`. This info is used in comments, mentions etc.
@@ -67,8 +68,11 @@ type Storage = {
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
-  // id?: string,  // Accessible through `user.id`
-  // info?: Json,  // Accessible through `user.info`
+  id?: string;
+  info?: {
+    name?: string;
+    picture?: string;
+  }; // Accessible through `user.info`
 };
 
 // Optionally, the type of custom events broadcast and listened to in this
